82bdf5ccbfc306a61c429771202c9ec7bb3a2684
Joe <Joe.Scott-Hartley@Opencastsoftware.com>
2025-10-23 15:26:47 +0100

    refactor: rename playerOnePoints variable
    Signed-off-by: Joe <Joe.Scott-Hartley@Opencastsoftware.com>

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -1,7 +1,7 @@
 
 public class TennisGame2 implements TennisGame
 {
-    public int P1point = 0;
+    public int playerOnePoints = 0;
     public int P2point = 0;
     
     public String P1res = "";
@@ -16,32 +16,32 @@ public class TennisGame2 implements TennisGame
 
     public String getScore(){
         String score = "";
-        if (P1point == P2point && P1point < 4)
+        if (playerOnePoints == P2point && playerOnePoints < 4)
         {
-            if (P1point==0)
+            if (playerOnePoints==0)
                 score = "Love";
-            if (P1point==1)
+            if (playerOnePoints==1)
                 score = "Fifteen";
-            if (P1point==2)
+            if (playerOnePoints==2)
                 score = "Thirty";
             score += "-All";
         }
-        if (P1point==P2point && P1point>=3)
+        if (playerOnePoints==P2point && playerOnePoints>=3)
             score = "Deuce";
         
-        if (P1point > 0 && P2point==0)
+        if (playerOnePoints > 0 && P2point==0)
         {
-            if (P1point==1)
+            if (playerOnePoints==1)
                 P1res = "Fifteen";
-            if (P1point==2)
+            if (playerOnePoints==2)
                 P1res = "Thirty";
-            if (P1point==3)
+            if (playerOnePoints==3)
                 P1res = "Forty";
             
             P2res = "Love";
             score = P1res + "-" + P2res;
         }
-        if (P2point > 0 && P1point==0)
+        if (P2point > 0 && playerOnePoints==0)
         {
             if (P2point==1)
                 P2res = "Fifteen";
@@ -54,11 +54,11 @@ public class TennisGame2 implements TennisGame
             score = P1res + "-" + P2res;
         }
         
-        if (P1point>P2point && P1point < 4)
+        if (playerOnePoints>P2point && playerOnePoints < 4)
         {
-            if (P1point==2)
+            if (playerOnePoints==2)
                 P1res="Thirty";
-            if (P1point==3)
+            if (playerOnePoints==3)
                 P1res="Forty";
             if (P2point==1)
                 P2res="Fifteen";
@@ -66,34 +66,34 @@ public class TennisGame2 implements TennisGame
                 P2res="Thirty";
             score = P1res + "-" + P2res;
         }
-        if (P2point>P1point && P2point < 4)
+        if (P2point>playerOnePoints && P2point < 4)
         {
             if (P2point==2)
                 P2res="Thirty";
             if (P2point==3)
                 P2res="Forty";
-            if (P1point==1)
+            if (playerOnePoints==1)
                 P1res="Fifteen";
-            if (P1point==2)
+            if (playerOnePoints==2)
                 P1res="Thirty";
             score = P1res + "-" + P2res;
         }
         
-        if (P1point > P2point && P2point >= 3)
+        if (playerOnePoints > P2point && P2point >= 3)
         {
             score = "Advantage player1";
         }
         
-        if (P2point > P1point && P1point >= 3)
+        if (P2point > playerOnePoints && playerOnePoints >= 3)
         {
             score = "Advantage player2";
         }
         
-        if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
+        if (playerOnePoints>=4 && P2point>=0 && (playerOnePoints-P2point)>=2)
         {
             score = "Win for player1";
         }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
+        if (P2point>=4 && playerOnePoints>=0 && (P2point-playerOnePoints)>=2)
         {
             score = "Win for player2";
         }
@@ -119,7 +119,7 @@ public class TennisGame2 implements TennisGame
     }
     
     public void P1Score(){
-        P1point++;
+        playerOnePoints++;
     }
     
     public void P2Score(){
