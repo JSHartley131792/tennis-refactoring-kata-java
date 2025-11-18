0acee17cad7528072aff597dc2b27cd95ddec33d
Joe <Joe.Scott-Hartley@Opencastsoftware.com>
2025-11-18 12:54:48 +0000

    refactor: move result string to object query
    Signed-off-by: Joe <Joe.Scott-Hartley@Opencastsoftware.com>

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -3,24 +3,23 @@ public class TennisGame2 implements TennisGame {
     public class Player {
         String name;
         int points;
-        public Player(String playerName, int playerPoints) {
+        String result;
+        public Player(String playerName, int playerPoints, String resultString) {
             this.name = playerName;
             this.points = playerPoints;
+            this.result = resultString;
         }
     }
 
-    public String playerOneResult = "";
-    public String playerTwoResult = "";
     public Player playerOne;
     public Player playerTwo;
 
     public TennisGame2(String player1Name, String player2Name) {
-        this.playerOne = new Player(player1Name, 0);
-        this.playerTwo = new Player(player2Name, 0);
+        this.playerOne = new Player(player1Name, 0, "");
+        this.playerTwo = new Player(player2Name, 0, "");
     }
 
     public String getScore() {
-        String score = "";
         if (playerOne.points == playerTwo.points) {
             if (playerOne.points < 4) {
                 if (playerOne.points == 0)
@@ -34,26 +33,30 @@ public class TennisGame2 implements TennisGame {
                 return "Deuce";
             }
         } else if (playerOne.points > playerTwo.points) {
-            return leadingScore(playerOne, playerTwo);
-        } else {
-            return leadingScore(playerTwo, playerOne);
-        }
-        return score;
-    }
-
-    public String leadingScore(Player leadingPlayer,Player losingPlayer) {
-        if (leadingPlayer.points >= 4 && losingPlayer.points >= 0 && (leadingPlayer.points - losingPlayer.points) >= 2) {
-                return winForPlayer(leadingPlayer.name);
-            } else if (losingPlayer.points >= 3) {
-                return advantageForPlayer(leadingPlayer.name);
-            } else if (leadingPlayer.points > 0 && losingPlayer.points == 0) {
-                return leadingToLove(leadingPlayer.points, leadingPlayer.name);
+            if (playerOne.points >= 4 && playerTwo.points >= 0 && (playerOne.points - playerTwo.points) >= 2) {
+                return winForPlayer(playerOne.name);
+            } else if (playerTwo.points >= 3) {
+                return advantageForPlayer(playerOne.name);
+            } else if (playerOne.points > 0 && playerTwo.points == 0) {
+                leadingToLove(playerOne, playerTwo);
             } else {
-                return leadingInGame(leadingPlayer, losingPlayer);
+                leadingInGame(playerOne, playerTwo);
             }
+        } else {
+            if (playerTwo.points >= 4 && playerOne.points >= 0 && (playerTwo.points - playerOne.points) >= 2) {
+                return winForPlayer(playerTwo.name);
+            } else if (playerOne.points >= 3) {
+                return advantageForPlayer(playerTwo.name);
+            } else if (playerTwo.points > 0 && playerOne.points == 0) {
+                leadingToLove(playerTwo, playerOne);
+            } else {
+                leadingInGame(playerTwo, playerOne);
+            }
+        }
+        return playerOne.result + "-" + playerTwo.result;
     }
 
-    public String leadingInGame(Player leadingPlayer, Player losingPlayer) {
+    public void leadingInGame(Player leadingPlayer, Player losingPlayer) {
         String leadingResult = "";
         String losingResult = "";
         if (leadingPlayer.points == 2)
@@ -64,11 +67,8 @@ public class TennisGame2 implements TennisGame {
             losingResult = "Fifteen";
         if (losingPlayer.points == 2)
             losingResult = "Thirty";
-        if(leadingPlayer.equals(playerOne)) {
-            return leadingResult + "-" + losingResult;
-        } else {
-            return losingResult + "-" + leadingResult;
-        }
+        leadingPlayer.result = leadingResult;
+        losingPlayer.result = losingResult;
     }
 
     public String winForPlayer(String playerName) {
@@ -79,20 +79,17 @@ public class TennisGame2 implements TennisGame {
         return "Advantage " + playerName;
     }
 
-    public String leadingToLove(int leadingPoints, String leadingPlayerName) {
+    public void leadingToLove(Player leadingPlayer, Player losingPlayer) {
         String leadingResult;
-        if (leadingPoints == 1)
+        if (leadingPlayer.points == 1)
             leadingResult = "Fifteen";
-        else if (leadingPoints == 2)
+        else if (leadingPlayer.points == 2)
             leadingResult = "Thirty";
         else
             leadingResult = "Forty";
 
-        if (leadingPlayerName == playerOne.name) {
-            return leadingResult + "-Love";
-        } else {
-            return "Love-" + leadingResult;
-        }
+        leadingPlayer.result = leadingResult;
+        losingPlayer.result = "Love";
     }
 
     public void incrementPlayerScore(Player player) {
