77eb64dce1c2b8089e8d321f457c1d19f2d260f3
Joe <Joe.Scott-Hartley@Opencastsoftware.com>
2025-11-20 12:21:56 +0000

    refactor: remove unneeded statement in conditional
    Signed-off-by: Joe <Joe.Scott-Hartley@Opencastsoftware.com>

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -42,7 +42,7 @@ public class TennisGame2 implements TennisGame {
     }
 
     public String leadingScore(Player leadingPlayer, Player losingPlayer) {
-        if (leadingPlayer.points >= 4 && losingPlayer.points >= 0
+        if (leadingPlayer.points >= 4
                 && (leadingPlayer.points - losingPlayer.points) >= 2) {
             return winForPlayer(leadingPlayer.name);
         } else if (losingPlayer.points >= 3) {
