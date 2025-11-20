55448798aed24521dd3caa1b65d2edbc5e0d7fd4
Joe <Joe.Scott-Hartley@Opencastsoftware.com>
2025-11-20 13:48:13 +0000

    refactor: rename variable
    Signed-off-by: Joe <Joe.Scott-Hartley@Opencastsoftware.com>

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,8 +1,8 @@
 
 public class TennisGame3 implements TennisGame {
     
+    private int playerOneScore;
     private int p2;
-    private int p1;
     private String p1N;
     private String p2N;
 
@@ -13,21 +13,21 @@ public class TennisGame3 implements TennisGame {
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
+        if (playerOneScore < 4 && p2 < 4 && !(playerOneScore + p2 == 6)) {
             String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+            s = p[playerOneScore];
+            return (playerOneScore == p2) ? s + "-All" : s + "-" + p[p2];
         } else {
-            if (p1 == p2)
+            if (playerOneScore == p2)
                 return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            s = playerOneScore > p2 ? p1N : p2N;
+            return ((playerOneScore-p2)*(playerOneScore-p2) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
     
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.playerOneScore += 1;
         else
             this.p2 += 1;
         
