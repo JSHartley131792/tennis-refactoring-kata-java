564bb9e0d11a2a66b9a97ed73c37799e4fa996b4
Joe <Joe.Scott-Hartley@Opencastsoftware.com>
2025-11-20 13:59:12 +0000

    chore: format document
    Signed-off-by: Joe <Joe.Scott-Hartley@Opencastsoftware.com>

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,6 +1,6 @@
 
 public class TennisGame3 implements TennisGame {
-    
+
     private int playerOneScore;
     private int playerTwoScore;
     private String playerOneName;
@@ -11,26 +11,29 @@ public class TennisGame3 implements TennisGame {
         this.playerTwoName = playerTwoName;
     }
 
-    String[] scoreStringArray = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
+    String[] scoreStringArray = new String[] { "Love", "Fifteen", "Thirty", "Forty" };
 
     public String getScore() {
         if (playerOneScore < 4 && playerTwoScore < 4 && !(playerOneScore + playerTwoScore == 6)) {
             String scoreString = scoreStringArray[playerOneScore];
-            return (playerOneScore == playerTwoScore) ? scoreString + "-All" : scoreString + "-" + scoreStringArray[playerTwoScore];
+            return (playerOneScore == playerTwoScore) ? scoreString + "-All"
+                    : scoreString + "-" + scoreStringArray[playerTwoScore];
         } else {
             if (playerOneScore == playerTwoScore)
                 return "Deuce";
             String leadingPlayer = playerOneScore > playerTwoScore ? playerOneName : playerTwoName;
-            return ((playerOneScore-playerTwoScore)*(playerOneScore-playerTwoScore) == 1) ? "Advantage " + leadingPlayer : "Win for " + leadingPlayer;
+            return ((playerOneScore - playerTwoScore) * (playerOneScore - playerTwoScore) == 1)
+                    ? "Advantage " + leadingPlayer
+                    : "Win for " + leadingPlayer;
         }
     }
-    
+
     public void wonPoint(String playerName) {
         if (playerName == "player1")
             this.playerOneScore += 1;
         else
             this.playerTwoScore += 1;
-        
+
     }
 
 }
